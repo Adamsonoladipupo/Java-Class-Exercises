9eb2b83c31b19da3e0481bac2f8285c7371b9b16
AbdulwahabAdamson <adamsonoladipupo@gmail.com>
2025-09-09 17:36:39 +0100

    Add SwappingValues.java file

---
+++ SwappingValues.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SwappingValues{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter first number: ");
+int a = input.nextInt();
+System.out.print("Enter second number: ");
+int b = input.nextInt();
+
+// swapping computation
+
+
+a = b + 0;
+b = a + 0;
+
+
+System.out.printf("first number is now %d, second number is now %d", a, b);
+
+}
+}
