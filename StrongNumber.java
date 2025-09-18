4d048525619600cdc760c9e9950b7a5715582ca6
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-18 05:57:07 +0100

    updating StrongNumber.java

--- StrongNumber.java
+++ StrongNumber.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class StrongNumber{
 public static void main(String[] args){
-
+int initialInput= 0;
 Scanner input = new Scanner(System.in);
 System.out.print("Enter a number: ");
 int number = input.nextInt();
+initialInput = number;
 
 // Solving for factorial of number
 
@@ -22,6 +23,7 @@ System.out.printf("The factorial of %d is %d", number, factorial);
 // computing the logic for determing StrongNumbers
 int seperatedNumber = 0;
 int factorial = 1;
+int sumFactorials = 0;
 while (number%10!=0 || number/10!=0){
 seperatedNumber = number % 10;
 	for (int i=1; i<=seperatedNumber; i++){
@@ -29,11 +31,18 @@ seperatedNumber = number % 10;
 	System.out.printf("factorial iteration %d %n", i);
 	}
 	System.out.printf("factorial %d is %d %n", number, factorial);
+	sumFactorials += factorial ;
+	factorial = 1;
 	number = number /10;
 	System.out.printf("seperation iteration for %d %n", number);
+	
 }
-
-System.out.printf("number %d and factorial %d seperated number %d", number, factorial, seperatedNumber);
+System.out.printf("Sum of Factorials %d %n ", sumFactorials);
+if (initialInput == sumFactorials){
+	System.out.printf("%d is a strong number ", initialInput);
+} else {
+	System.out.printf("%d is not a strong number ", initialInput);
+}
 
 
 
