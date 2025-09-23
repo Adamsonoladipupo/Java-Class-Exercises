f3c33bbc18b1804757cb30afeede94a2a101b2ee
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-23 13:44:56 +0100

    new update

---
+++ AsterisksTriangles.java
@@ -0,0 +1,22 @@
+/*
+Pseudocode
+- declear a variable and store * in it.
+- 
+
+*/
+
+public class AsterisksTriangles{
+public static void main(String[] args){
+// char asterisk = '*';
+int i = 0;
+int counter = 0;
+
+while ( counter < 5){
+System.out.print("1 ");
+counter++;
+ 
+}
+
+
+}
+}
