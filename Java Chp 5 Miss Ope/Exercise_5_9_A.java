f6698e81b6a855c2e4110db7a18482c6a3f1ba53
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-28 14:39:31 +0100

    new update

---
+++ Java Chp 5 Miss Ope/Exercise_5_9_A.java
@@ -0,0 +1,19 @@
+public class Exercise_5_9_A{
+	public static void main(String[] args){
+
+
+	String docstings = """
+		Find and correct the error(s) in each 
+		of the following segments of code:
+
+		while (i = 1; i <= 10, i+)
+		System.out.println(i); 
+
+	""";
+	System.out.print(docstings);
+	int i = 1;
+	for (i = 1; i <= 10; i++){
+		System.out.println(i); 
+	}
+}
+}
---
+++ Java Chp 5 Miss Ope/Exercise_5_9_B.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercise_5_9_B{
+	public static void main(String[] args){
+
+
+	String docstings = """
+		Find and correct the error(s) in each 
+		of the following segments of code:
+
+		The following code should print whether 
+		an integer value is negative or zero:
+		
+		switch (value) {
+			case value < 0:
+   				System.out.println("Negative");
+  			case 0:
+  		 		System.out.println("Zero");
+   		}
+
+	""";
+	System.out.print(docstings);
+
+	Scanner inputCollector = new Scanner(System.in);
+	System.out.print("Enter a number: ");
+	int value = inputCollector.nextInt();
+	switch (value) {
+		case value < 0:
+   			System.out.println("Negative");
+  		case 0:
+  		 	System.out.println("Zero");
+   	}
+}
+}
