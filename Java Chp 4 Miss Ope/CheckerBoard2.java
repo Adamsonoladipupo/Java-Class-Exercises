a070867ec2b2589a2d2d367dc892fca8404c3a8e
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-30 04:57:50 +0100

    new update

---
+++ Java Chp 4 Miss Ope/CheckerBoard2.java
@@ -0,0 +1,16 @@
+public class CheckerBoard2{
+	public static void main(String [] args){
+		for(int counter = 1; counter<=8; counter++){
+			System.out.print(" ");
+			System.out.println();
+			for(int count = 1; count<=8; count++){
+				System.out.print("* ");
+
+
+				
+			}
+		}
+
+
+	}
+}
