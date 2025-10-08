eac15645c9f1d9ebc3560dd9c9748d176e5ebdae
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-08 21:20:57 +0100

    new update

--- Morning Cup Cakes/morningCupCakes.java
+++ Morning Cup Cakes/morningCupCakes.java
@@ -2,25 +2,13 @@ public class morningCupCakes{
 	public static void main(String[] args){
 		
 	}
-	public static double inSmallest(int counter, double userInput){
-		double[] integers = new double[userInput];
-		for (int count = 0; count < userInput){
-			
-		}
-	}
 
-	public static double isAverage(int indexes, double userInput){
-		System.out.print("Enter number of array index");
-		int indexes = input.nextInt();
+
+	public static double isAverage(double[] arrayValues){
 		double result = 0;
-		double[] array = new double[indexes];
-		for (int count = 0; count < indexes; count++){
-			System.out.printf("Enter number for index: %d: ", count);
-			array[count] = input.nextDouble();
-			result += array[count];
-		} 
-		result = result / indexes;
-		//System.out.printf("The average is: %f", result);
+		for (int count = 0; count < arrayValues.length; count++){
+			result += arrayValues[count];
+		} result = result / arrayValues.length;
 
 		return result;
 	}
--- Morning Cup Cakes/morningCupCakesTest.java
+++ Morning Cup Cakes/morningCupCakesTest.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class morningCupCakesTest{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter indexes: ");
-		int index = input.nextInt();
 
-		System.out.print("Enter numbers: ");
-		double uInput = input.nextInt();
-
-		double rsult = morningCupCakes.isAverage(index, uInput);
-		System.out.print(rsult);
+		double[] integers = {2.0, 2, 2, 2.0, 2};
+		double average = morningCupCakes.isAverage(integers);
 		
+		System.out.print(average);
  		
 
 	}
