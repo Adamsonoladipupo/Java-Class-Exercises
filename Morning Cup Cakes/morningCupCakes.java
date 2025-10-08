d037c6ae00f95e5db4c64425e4cb6acefa3104d3
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-08 21:50:23 +0100

    new update

--- Morning Cup Cakes/morningCupCakes.java
+++ Morning Cup Cakes/morningCupCakes.java
@@ -3,6 +3,16 @@ public class morningCupCakes{
 		
 	}
 
+	public static int isSmallest(int[] integers){
+		int smallest = integers[0];
+		for (int count = 1; count < integers.length; count++){
+			//System.out.println(integers[count]);
+			if (integers[0] > integers[count]){
+				smallest = integers[count];
+			}
+		}
+		return smallest;
+	}
 
 	public static double isAverage(double[] arrayValues){
 		double result = 0;
--- Morning Cup Cakes/morningCupCakesTest.java
+++ Morning Cup Cakes/morningCupCakesTest.java
@@ -4,10 +4,14 @@ public class morningCupCakesTest{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
 
-		double[] integers = {2.0, 2, 2, 2.0, 2};
-		double average = morningCupCakes.isAverage(integers);
+		int[] integers = {2, 1, 3, 4, -1};
+		int eTooSmall = morningCupCakes.isSmallest(integers);
 		
-		System.out.print(average);
+
+		
+		//double average = morningCupCakes.isAverage(integers);
+		
+		System.out.print(eTooSmall);
  		
 
 	}
