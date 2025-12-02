359f7501a25602f191eb16d085155c6f28500573
OlaAdams <adamsonoladipupo@gmail.com>
2025-12-02 21:29:58 +0100

    new update

--- Banking System/Banking System/src/bank/Bank.java
+++ Banking System/Banking System/src/bank/Bank.java
@@ -1,4 +1,13 @@
 package bank;
 
 public class Bank {
+    private int accounts = 0;
+
+    public int numberOfAccount() {
+        return accounts;
+    }
+
+    public void createAccount(String name, int accountNumber, String password) {
+        accounts++;
+    }
 }
--- Banking System/Banking System/test/bank/BankTest.java
+++ Banking System/Banking System/test/bank/BankTest.java
@@ -1,5 +1,28 @@
 package bank;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankTest {
-    
+    Bank bank;
+
+    @BeforeEach
+    public void setUp(){
+        bank = new Bank();
+    }
+
+    @Test
+    public void bank_does_not_have_any_acount_test(){
+        assertEquals(0, bank.numberOfAccount());
+    }
+
+    @Test
+    public void create_a_new_account_test(){
+        bank.createAccount("Bayo", 012222222, "PassWord123");
+        assertEquals(1, bank.numberOfAccount());
+    }
+
+
 }
